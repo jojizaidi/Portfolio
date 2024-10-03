857451554d4449574ce9bfbc9893c24f9d2d1960
Zohaib Zaidi <m.zohaibzaidi@gmail.com>
2024-10-03 10:50:50 +0100

    Updated ballroom build

--- BallroomBuild/server.py
+++
@@ -1,8 +0,0 @@
-import http.server
-import ssl
-
-httpd = http.server.HTTPServer(('127.0.0.1', 443), http.server.SimpleHTTPRequestHandler)
-ctx = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
-ctx.load_cert_chain(certfile='./server.pem')
-httpd.socket = ctx.wrap_socket(httpd.socket, server_side=True)
-httpd.serve_forever()
